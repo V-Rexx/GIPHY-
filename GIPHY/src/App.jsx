7bfa224284920112d50a82dc20552dfce27a9393
Vigoraj_bty <bvigoraj@gmail.com>
2025-09-14 23:18:49 +0530

    Update

--- GIPHY/src/App.jsx
+++ GIPHY/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Buttons from './components/Buttons'
 
 function App() {
   
 
   return (
     <>
-     
+      <Buttons />
     </>
   )
 }
--- GIPHY/src/components/Buttons.jsx
+++ GIPHY/src/components/Buttons.jsx
@@ -1,7 +1,10 @@
+
+
+
 export default function Buttons(){
     return(
         <div>
-            
+            <button>COUNT is:</button>
         </div>
     )
 }

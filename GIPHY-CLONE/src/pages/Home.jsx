187c16f5acbc2822a03fb2e51cf5ec3fb5e15bbb
Vigoraj_bty <bvigoraj@gmail.com>
2025-10-09 21:47:32 +0530

    Update Home.jsx

--- GIPHY-CLONE/src/pages/Home.jsx
+++ GIPHY-CLONE/src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='relative flex gap-4 justify-between items-center mb-2'>
+
+      <div>Home</div>
+
+    </div>
+    
   )
 }
 
